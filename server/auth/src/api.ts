a2e72323f6b1702788bc59ead576d6030d1f493d
alu0101048369 <accounts@migueldorta.com>
2023-11-22 20:16:45 +0000

    Fix CORS

--- server/auth/src/api.ts
+++ server/auth/src/api.ts
@@ -1,53 +1,46 @@
 import express from "express";
+import cors from "cors";
 import { findUserByEmail, writeUser } from "./db";
 import { check, hash } from "./bcrypt";
 
 export function start() {
   const app = express();
   app.use(express.json());
+  app.use(cors());
 
-  const cors = require("cors")({
-    origin: "http://localhost",
-    methods: ["GET", "POST", "OPTIONS"],
+  app.post('/login', async (req, resp) => {
+    const user = await findUserByEmail(req.body.email);
+    if (!user) {
+      resp.status(404).json({ ok: false, error: "Invalid email or password"});
+      return;
+    }
+
+    if (!(await check(req.body.pass, user.pass))) {
+      resp.status(404).json({ ok: false, error: "Invalid email or password"});
+      return;
+    }
+
+    resp.status(200).json({ ok: true, message: 'Successful Login' });
   });
 
-  app.post('/login', (req, resp) => {
-    return cors(req, resp, async () => {
-      const user = await findUserByEmail(req.body.email);
-      if (!user) {
-        resp.status(404).json({ ok: false, error: "Invalid email or password"});
-        return;
-      }
+  app.post('/register', async (req, resp) => {
+    const body = req.body;
 
-      if (!(await check(req.body.pass, user.pass))) {
-        resp.status(404).json({ ok: false, error: "Invalid email or password"});
-        return;
-      }
+    if (await findUserByEmail(req.body.email)) {
+      resp.status(400).json({ ok: false, error: "User with email already exist"});
+      return;
+    }
 
-      resp.status(200).json({ ok: true, message: 'Successful Login' });
+    // TODO check body, check user has been written successfully
+    await writeUser({
+      email: body.email,
+      groups: {},
+      image: body.image,
+      name: body.name,
+      pass: await hash(body.pass)
     });
-  });
 
-  app.post('/register', (req, resp) => {
-    return cors(req, resp, async () => {
-      const body = req.body;
-
-      if (await findUserByEmail(req.body.email)) {
-        resp.status(400).json({ ok: false, error: "User with email already exist"});
-        return;
-      }
-
-      // TODO check body, check user has been written successfully
-      await writeUser({
-        email: body.email,
-        groups: {},
-        image: body.image,
-        name: body.name,
-        pass: await hash(body.pass)
-      });
-
-      resp.status(201).json({ ok: true, message: 'Successful Register' });
-    });
+    resp.status(201).json({ ok: true, message: 'Successful Register' });
   });
 
   app.listen("7480");
