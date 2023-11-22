14fd68d98e7fda465b06c88e272687de1393636e
Miguel-Dorta <contact@migueldorta.com>
2023-11-22 19:37:21 +0000

    Implement CORS

--- server/auth/src/api.ts
+++ server/auth/src/api.ts
@@ -6,39 +6,48 @@ export function start() {
   const app = express();
   app.use(express.json());
 
-  app.post('/login', async (req, resp) => {
-    const user = await findUserByEmail(req.body.email);
-    if (!user) {
-      resp.status(404).json({ ok: false, error: "Invalid email or password"});
-      return;
-    }
-
-    if (!(await check(req.body.pass, user.pass))) {
-      resp.status(404).json({ ok: false, error: "Invalid email or password"});
-      return;
-    }
-
-    resp.status(200).json({ ok: true, message: 'Successful Login' });
+  const cors = require("cors")({
+    origin: "http://localhost",
+    methods: ["GET", "POST", "OPTIONS"],
   });
 
-  app.post('/register', async (req, resp) => {
-    const body = req.body;
+  app.post('/login', (req, resp) => {
+    return cors(req, resp, async () => {
+      const user = await findUserByEmail(req.body.email);
+      if (!user) {
+        resp.status(404).json({ ok: false, error: "Invalid email or password"});
+        return;
+      }
 
-    if (await findUserByEmail(req.body.email)) {
-      resp.status(400).json({ ok: false, error: "User with email already exist"});
-      return;
-    }
+      if (!(await check(req.body.pass, user.pass))) {
+        resp.status(404).json({ ok: false, error: "Invalid email or password"});
+        return;
+      }
 
-    // TODO check body, check user has been written successfully
-    await writeUser({
-      email: body.email,
-      groups: {},
-      image: body.image,
-      name: body.name,
-      pass: await hash(body.pass)
+      resp.status(200).json({ ok: true, message: 'Successful Login' });
     });
+  });
 
-    resp.status(201).json({ ok: true, message: 'Successful Register' });
+  app.post('/register', (req, resp) => {
+    return cors(req, resp, async () => {
+      const body = req.body;
+
+      if (await findUserByEmail(req.body.email)) {
+        resp.status(400).json({ ok: false, error: "User with email already exist"});
+        return;
+      }
+
+      // TODO check body, check user has been written successfully
+      await writeUser({
+        email: body.email,
+        groups: {},
+        image: body.image,
+        name: body.name,
+        pass: await hash(body.pass)
+      });
+
+      resp.status(201).json({ ok: true, message: 'Successful Register' });
+    });
   });
 
   app.listen("7480");
