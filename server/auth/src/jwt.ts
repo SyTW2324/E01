4243d35f503e672a5e13c4fa03a99db0f5ce74f9
Miguel-Dorta <contact@migueldorta.com>
2023-11-29 16:54:20 +0000

    Implemented JWT and field validations in Auth Backend

--- server/auth/src/jwt.ts
+++ server/auth/src/jwt.ts
@@ -4,19 +4,19 @@ import { getKeys } from "./crypto_utils";
 
 const expiresAfter = 1 * 60 * 60;
 
-async function generateJWT(user: User): Promise<{token: string, publicKey: string}> {
+export async function generateJWT(uid: string, user: User): Promise<{token: string, publicKey: string}> {
     const { publicKey, privateKey } = await getKeys();
     const token = sign({
         email: user.email,
         image: user.image,
         name: user.name,
-        uid: user._id?.toString(),
+        uid,
     }, privateKey, {
         algorithm: "ES256",
         expiresIn: expiresAfter,
         issuer: "sharethecost",
         notBefore: 0,
-        subject: user._id?.toString()
+        subject: uid
     });
     return {token, publicKey};
 }
---
+++ server/auth/src/validation.ts
@@ -0,0 +1,5 @@
+export function isValidEmail(email: unknown): boolean {
+  return typeof email === "string" && !!email.match(
+    /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+  );
+}
