136ebcf31723aa4c545c7bfc6dd839a7c3198c75
Miguel-Dorta <contact@migueldorta.com>
2023-12-21 02:35:26 +0000

    Fix upgrade SvelteKit

--- public/src/routes/+layout.svelte
+++ public/src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import Navbar from '$lib/components/navbar.svelte';
 </script>
-
-<Navbar />
 
 <main class="bg-neth-xdark min-h-screen w-full">
 	<slot />
