58ea868d4eecfdb0ccd50c498ee4e6979bdcd0df
Miguel-Dorta <contact@migueldorta.com>
2024-01-16 18:49:24 +0000

    View screen done

--- public/src/lib/components/transaction/window.svelte
+++ public/src/lib/components/transaction/window.svelte
@@ -1,24 +1,129 @@
 <script lang="ts">
 	import Window from "$lib/components/window.svelte";
+	
 	export let renderPriority: number;
+	let edit = false;
 </script>
 
 <Window title="Party Snacks" renderPriority={renderPriority}>
-	<div class="h-full bg-white text-black p-4">
-	  <h2 class="font-bold text-lg mb-4">Categories: Viaje, Comida</h2>
-  
-	  <strong class="block mb-2">Amount: 500€ (Sumar los payments)</strong>
-	  <strong class="block mb-2">Date: "TODAY"</strong>
-  
-	  <strong class="block mb-2">DebtShares:</strong>
-	  <ul class="list-disc pl-6 mb-4">
-		<li>Alejandro: 250€</li>
-		<li>Lucas: 250€</li>
-	  </ul>
-  
-	  <strong class="block mb-2">Payments:</strong>
-	  <ul class="list-disc pl-6">
-		<li>Miguel: 500€</li>
-	  </ul>
+	<div class="bg-white h-full text-black px-4 py-2">
+		<table class="table-auto text-left w-full">
+			<tr>
+				<th>Concept:</th>
+				<td class="pl-2">
+					<input
+						class="bg-transparent border-b px-1 w-full focus:outline-none focus:border-neth {edit ? "border-b" : "border-white"}"
+						id="transaction-concept"
+						type="text"
+						placeholder="Concept"
+						disabled={!edit}>
+				</td>
+			</tr>
+			<tr>
+				<th>Categories:</th>
+				<td class="pl-2">
+					<input
+						class="bg-transparent border-b px-1 w-full focus:outline-none focus:border-neth {edit ? "border-b" : "border-white"}"
+						id="transaction-categories"
+						type="text"
+						placeholder="Groceries, Restaurants"
+						disabled={!edit}>
+				</td>
+			</tr>
+			<tr>
+				<th>Date:</th>
+				<td class="pl-2">
+					<input
+						class="bg-transparent border-b px-1 w-full focus:outline-none focus:border-neth {edit ? "border-b" : "border-white"}"
+						id="transaction-date"
+						type="datetime-local"
+						disabled={!edit}>
+				</td>
+			</tr>
+			<tr>
+				<th>Amount:</th>
+				<td class="pl-2">
+					<input
+						class="appearance-none bg-transparent border-b px-1 text-right w-full focus:outline-none focus:border-neth border-white"
+						id="transaction-amount"
+						type="number"
+						placeholder="24.98"
+						disabled>
+				</td>
+				<td>€</td>
+			</tr>
+			<tr>
+				<th class="font-normal pl-4">Alejandro:</th>
+				<td class="pl-2">
+					<input
+						class="appearance-none bg-transparent border-b px-1 text-right w-full focus:outline-none focus:border-neth {edit ? "border-b" : "border-white"}"
+						id="transaction-amount-Alejandro"
+						type="number"
+						placeholder="24.98"
+						disabled={!edit}>
+				</td>
+				<td>€</td>
+			</tr>
+			<tr>
+				<th class="font-normal pl-4">Lucas:</th>
+				<td class="pl-2">
+					<input
+						class="appearance-none bg-transparent border-b px-1 text-right w-full focus:outline-none focus:border-neth {edit ? "border-b" : "border-white"}"
+						id="transaction-amount-Lucas"
+						type="number"
+						placeholder="24.98"
+						disabled={!edit}>
+				</td>
+				<td>€</td>
+			</tr>
+			<tr>
+				<th class="font-normal pl-4">Miguel:</th>
+				<td class="pl-2">
+					<input
+						class="appearance-none bg-transparent border-b px-1 text-right w-full focus:outline-none focus:border-neth {edit ? "border-b" : "border-white"}"
+						id="transaction-amount-Miguel"
+						type="number"
+						placeholder="24.98"
+						disabled={!edit}>
+				</td>
+				<td>€</td>
+			</tr>
+			<tr>
+				<th>Shares</th>
+			</tr>
+			<tr>
+				<th class="font-normal pl-4">Alejandro:</th>
+				<td class="pl-2">
+					<input
+						class="appearance-none bg-transparent border-b px-1 text-right w-full focus:outline-none focus:border-neth {edit ? "border-b" : "border-white"}"
+						id="transaction-amount-Alejandro"
+						type="number"
+						placeholder="1"
+						disabled={!edit}>
+				</td>
+			</tr>
+			<tr>
+				<th class="font-normal pl-4">Lucas:</th>
+				<td class="pl-2">
+					<input
+						class="appearance-none bg-transparent border-b px-1 text-right w-full focus:outline-none focus:border-neth {edit ? "border-b" : "border-white"}"
+						id="transaction-amount-Lucas"
+						type="number"
+						placeholder="1"
+						disabled={!edit}>
+				</td>
+			</tr>
+			<tr>
+				<th class="font-normal pl-4">Miguel:</th>
+				<td class="pl-2">
+					<input
+						class="appearance-none bg-transparent border-b px-1 text-right w-full focus:outline-none focus:border-neth {edit ? "border-b" : "border-white"}"
+						id="transaction-amount-Miguel"
+						type="number"
+						placeholder="1"
+						disabled={!edit}>
+				</td>
+			</tr>
+		</table>
 	</div>
 </Window>
--- public/src/lib/components/window.svelte
+++ public/src/lib/components/window.svelte
@@ -3,7 +3,7 @@
 	export let renderPriority: number;
 </script>
 
-<div class="w-full md:p-3 md:w-1/2 lg:w-1/3 {renderPriority > 0 ? `hidden ${renderPriority < 2 ? "md" : "lg"}:block` : ""}">
+<div class="w-full md:p-3 md:w-1/2 lg:w-1/3 {renderPriority > 0 ? `hidden ${renderPriority < 2 ? "md:block" : "lg:block"}` : ""}">
 	<div class="bg-neth border border-neth-600 flex flex-col h-full shadow text-white w-full md:rounded">
 		<h2 class="mb-3 mt-2 text-center text-xl">{title}</h2>
 		<slot />
