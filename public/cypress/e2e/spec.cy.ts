7236484952a5621cf8b1044af50c3352822c716e
Lucas Pérez Rosario <alu0101399928@ull.edu.es>
2024-01-17 21:43:35 +0000

    Fixing e2e

--- public/cypress/e2e/spec.cy.ts
+++ public/cypress/e2e/spec.cy.ts
@@ -4,50 +4,56 @@ describe('template spec', () => {
   it('login', () => {
     cy.visit('https://sharethecost.dorta.win/login')
 
-    cy.wait(1000);
-    cy.get('input[type="email"]').type('me@migueldorta.com');
-    cy.get('input[type="password"]').type('1q2w3e4r');
-    cy.get('button[type="submit"]').click();
+    cy.wait(2000);
+    cy.get('input[id="login-email"]').type('lpr@me.com');
+    cy.get('input[id="login-pass"]').type('123456789Abcd@');
+    cy.get('button[type="button"]').click();
     //cy.request('https://sharethecost.dorta.win/api/auth/api/auth/').its('status').should('be.equal', 200);
   })
   it('register', () => {
     cy.visit('https://sharethecost.dorta.win/register')
 
     cy.wait(1000);
-    cy.get('input[id="name"]').type('LucasPR');
-    cy.get('input[id="email"]').type('me@lucaspr.com');
-    cy.get('input[id="password"]').type('q1w2e3r4');
-    cy.get('input[id="password-confirmation"]').type('q1w2e3r4');
-    cy.get('button[type="submit"]').click();
+    cy.get('input[id="register-name"]').type('LucasPR');
+    cy.get('input[id="register-email"]').type(`${new Date().getTime().toString(36)}@invalid.user`);
+    cy.get('input[id="register-pass"]').type('123456789Abcd@');
+    cy.get('input[id="register-pass-confirm"]').type('123456789Abcd@');
+    cy.get('button[type="button"]').click();
   })
 
   it('invalid login', () => {
     cy.visit('https://sharethecost.dorta.win/login')
 
     cy.wait(1000);
-    cy.get('input[type="email"]').type('me@migueldorta.com');
-    cy.get('input[type="password"]').type('1q2w');
-    cy.get('button[type="submit"]').click();
+    cy.get('input[id="login-email"]').type('me@mldorta.com');
+    cy.get('input[id="login-pass"]').type('1q2wjjj');
+    cy.on('window:alert', (mensaje) => {
+
+      expect(mensaje).to.equal('Error: login: Invalid email or password');
+    });
+
+    cy.get('button[type="button"]').click();
+
   })
   it('invalid register user already exist', () => {
     cy.visit('https://sharethecost.dorta.win/register')
 
     cy.wait(1000);
-    cy.get('input[id="name"]').type('LucasPR');
-    cy.get('input[id="email"]').type('me@lucaspr.com');
-    cy.get('input[id="password"]').type('q1w2e3r4');
-    cy.get('input[id="password-confirmation"]').type('q1w2e3r4');
-    cy.get('button[type="submit"]').click();
+    cy.get('input[id="register-name"]').type('LucasPR');
+    cy.get('input[id="register-email"]').type('me@lucaspr.com');
+    cy.get('input[id="register-pass"]').type('q1w2e3r4');
+    cy.get('input[id="register-pass-confirm"]').type('q1w2e3r4');
+    cy.get('button[type="button"]').click();
   })  
   it('invalid register not macth passwords', () => {
     cy.visit('https://sharethecost.dorta.win/register')
 
     cy.wait(1000);
-    cy.get('input[id="name"]').type('LucasPR');
-    cy.get('input[id="email"]').type('me@lucasper.com');
-    cy.get('input[id="password"]').type('q1w2e3r4');
-    cy.get('input[id="password-confirmation"]').type('other');
-    cy.get('button[type="submit"]').click();
+    cy.get('input[id="register-name"]').type('LucasPR');
+    cy.get('input[id="register-email"]').type('me@lucasper.com');
+    cy.get('input[id="register-pass"]').type('123456789Abcd@');
+    cy.get('input[id="register-pass-confirm"]').type('other');
+    cy.get('button[type="button"]').click();
   }) 
   
 })
