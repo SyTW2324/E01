e6edb619389724723fc0057d3efea179d87ffc12
Miguel-Dorta <contact@migueldorta.com>
2024-01-17 12:58:27 +0000

    Fix user not found stopping the service

--- server/auth/src/index.ts
+++ server/auth/src/index.ts
@@ -4,9 +4,11 @@ import { load } from "./config.js";
 import { init as initCrypto } from "./crypto_utils.js";
 import { connect } from "./db.js";
 import { init as initJWT } from "./jwt.js";
+import { init as initLog } from "./logger.js";
 
 async function main() {
   const config = await load("config/auth.json");
+  initLog(config);
   connect(config.db.uri);
   initCrypto(config);
   initBcrypt(config.hash.saltRounds);
