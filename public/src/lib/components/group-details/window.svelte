dba70abe28cf61ef702770ada6b50135cb0d550a
Miguel-Dorta <contact@migueldorta.com>
2024-01-17 20:31:36 +0000

    Add summary money calculations

--- public/src/lib/components/group-details/window.svelte
+++ public/src/lib/components/group-details/window.svelte
@@ -3,21 +3,26 @@
     import Table from "$lib/components/group-details/table.svelte";
 	  import Window from "$lib/components/window.svelte";
     import type { Transaction } from "$lib/db/transactions";
+	import { calcSummary } from "$lib/money/summary";
 
     export let title: string;
     export let transactions: {[tid: string]: Transaction};
     export let selectedTran: string;
+    export let groupMembers: {[uid: string]: string};
     export let renderPriority: number;
+    const summary = Object.entries(calcSummary(transactions)).reduce((acc, val) => {
+        const uid = val[0];
+        const amount = val[1];
+        const username = groupMembers[uid];
+        acc[username] = amount;
+        return acc;
+    }, {} as {[username: string]: number})
 </script>
 
 <Window title={title} renderPriority={renderPriority}>
     <div class="h-full relative">
         <div class="absolute bg-white bottom-0 divide-gray-100 divide-y left-0 right-0 text-black top-0 overflow-y-auto">
-            <Table summary={{
-                "Alejandro": 250,
-                "Lucas": 250,
-                "Miguel": -500
-            }} />
+            <Table summary={summary} />
             <List
                 trans={Object.values(transactions).sort((t1, t2) => t2.date - t1.date)}
                 selectedTran={selectedTran} />
