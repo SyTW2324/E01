28ff1c9b1ff2ebea420ddec92debc84df3c81a97
Miguel-Dorta <contact@migueldorta.com>
2024-01-18 00:57:57 +0000

    Merge branch 'cypress-e2e-testing' into road-to-mvp
    Co-authored-by: Lucas Pérez Rosario <Lucasperez02@users.noreply.github.com>

--- public/cypress/e2e/spec.cy.ts
+++ public/cypress/e2e/spec.cy.ts
@@ -5,49 +5,63 @@ describe('template spec', () => {
     cy.visit('https://sharethecost.dorta.win/login')
 
     cy.wait(1000);
-    cy.get('input[type="email"]').type('me@migueldorta.com');
-    cy.get('input[type="password"]').type('1q2w3e4r');
-    cy.get('button[type="submit"]').click();
-    //cy.request('https://sharethecost.dorta.win/api/auth/api/auth/').its('status').should('be.equal', 200);
+    cy.get('input[id="login-email"]').type('lpr@me.com');
+    cy.get('input[id="login-pass"]').type('123456789Abcd@');
+    cy.get('button[type="button"]').click();
+    
   })
   it('register', () => {
     cy.visit('https://sharethecost.dorta.win/register')
 
     cy.wait(1000);
-    cy.get('input[id="name"]').type('LucasPR');
-    cy.get('input[id="email"]').type('me@lucaspr.com');
-    cy.get('input[id="password"]').type('q1w2e3r4');
-    cy.get('input[id="password-confirmation"]').type('q1w2e3r4');
-    cy.get('button[type="submit"]').click();
+    cy.get('input[id="register-name"]').type('LucasPR');
+    cy.get('input[id="register-email"]').type(`${new Date().getTime().toString(36)}@invalid.user`);
+    cy.get('input[id="register-pass"]').type('123456789Abcd@');
+    cy.get('input[id="register-pass-confirm"]').type('123456789Abcd@');
+    cy.get('button[type="button"]').click();
   })
 
   it('invalid login', () => {
     cy.visit('https://sharethecost.dorta.win/login')
 
     cy.wait(1000);
-    cy.get('input[type="email"]').type('me@migueldorta.com');
-    cy.get('input[type="password"]').type('1q2w');
-    cy.get('button[type="submit"]').click();
+    cy.get('input[id="login-email"]').type('me@mldorta.com');
+    cy.get('input[id="login-pass"]').type('1q2wjjj');
+
+    const alertShown = cy.stub().as("alertShown")
+    cy.wait(1000);
+    cy.on ('window:alert', alertShown)
+    cy.wait(1000);
+    cy.get('button[type="button"]').click()
+    cy.wait(1000);
+    cy.get("@alertShown").should("have.been.calledOnceWith")
+
   })
   it('invalid register user already exist', () => {
     cy.visit('https://sharethecost.dorta.win/register')
 
     cy.wait(1000);
-    cy.get('input[id="name"]').type('LucasPR');
-    cy.get('input[id="email"]').type('me@lucaspr.com');
-    cy.get('input[id="password"]').type('q1w2e3r4');
-    cy.get('input[id="password-confirmation"]').type('q1w2e3r4');
-    cy.get('button[type="submit"]').click();
+    cy.get('input[id="register-name"]').type('LucasPR');
+    cy.get('input[id="register-email"]').type('lpr@me.com');
+    cy.get('input[id="register-pass"]').type('123456789Abcd@');
+    cy.get('input[id="register-pass-confirm"]').type('123456789Abcd@');
+    const alertShown = cy.stub().as("alertShown")
+    cy.wait(1000);
+    cy.on ('window:alert', alertShown)
+    cy.wait(1000);
+    cy.get('button[type="button"]').click()
+    cy.wait(1000);
+    cy.get("@alertShown").should("have.been.calledOnceWith")
   })  
-  it('invalid register not macth passwords', () => {
+  it('invalid register not match passwords', () => {
     cy.visit('https://sharethecost.dorta.win/register')
 
     cy.wait(1000);
-    cy.get('input[id="name"]').type('LucasPR');
-    cy.get('input[id="email"]').type('me@lucasper.com');
-    cy.get('input[id="password"]').type('q1w2e3r4');
-    cy.get('input[id="password-confirmation"]').type('other');
-    cy.get('button[type="submit"]').click();
+    cy.get('input[id="register-name"]').type('LucasPR');
+    cy.get('input[id="register-email"]').type('me@lucasper.com');
+    cy.get('input[id="register-pass"]').type('123456789Abcd@');
+    cy.get('input[id="register-pass-confirm"]').type('other');
+    cy.get('button[type="button"]').click()
   }) 
   
 })
